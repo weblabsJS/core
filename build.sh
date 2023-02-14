950284d8e8dbc3e66e5be9164dfc5cdff76ec2c7
DevMayukh-STRTM <kismotechindia@gmail.com>
2023-02-14 23:40:40 +0530

    Updated package.json

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cp ./src/package.json ./dist
 cp README.MD ./dist
 git add ./src/package.json
 git add build.sh
-git add README.md
+git add README.MD
 git commit -m "Updated package.json"
 git push
 cd dist && npm publish --access public
