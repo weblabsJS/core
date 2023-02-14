9e91b3bcbeed002894ad7bc9af5be916fe9529b5
DevMayukh-STRTM <kismotechindia@gmail.com>
2023-02-14 13:25:58 +0530

    updated build script

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ tsc -p ./tsconfig.json
 rm ./dist/cdn.d.ts
 rm ./src/cdn.ts
 cp ./src/package.json ./dist
+git add ./src/package.json
+git commit -m "Updated package.json"
+git push
 cd dist && npm publish --access public
