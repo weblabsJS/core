dffbc426ce05f25f06a29a3e569abafd40a6ed86
DevMayukh-STRTM <kismotechindia@gmail.com>
2023-07-25 21:31:10 +0530

    Updated new features

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ cp README.MD ./dist
 git add ./src/package.json
 git add build.sh
 git add README.MD
+git add *
 git commit -m "Updated package.json"
 git push
 cd dist && npm publish --access public
