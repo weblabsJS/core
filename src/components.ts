b161f1ffa7770aac5866745f96210a68aedc39a6
DevMayukh-STRTM <kismotechindia@gmail.com>
2023-08-02 17:47:40 +0530

    Updated package.json

--- src/components.ts
+++ src/components.ts
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import { WebLabsElement, WebLabsChild } from './index'
+import { WebLabsElement, WebLabsChild, render } from './index'
 
 export function a(...children: WebLabsChild[]): WebLabsElement {
     return new WebLabsElement("a", ...children)
